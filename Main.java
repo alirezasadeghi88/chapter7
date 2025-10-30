e73ca06e330157245c7e4d794b36825fe1199a29
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-10-30 10:55:05 -0700

    R

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int[] list = {6 , 5 , 4 , 3 , 2 , 1 };
+		BinarySearch search = new BinarySearch();
+		int a = search.binarySearch(list , 3);
+		System.out.println(a);
+		
+	}
+}
